0b3ddc5dd7042820f3b726e3e3e8cee96cef2681
set123ed <set123ed@gmail.com>
2021-04-08 06:03:23 -0400

    update calendar page

--- NBAStats/NBAStats/Models/FavoritePlayer.cs
+++ NBAStats/NBAStats/Models/FavoritePlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.SQLite;
 using SQLite;
 
 namespace NBAStats.Models
--- NBAStats/NBAStats/Models/FavoriteTeam.cs
+++ NBAStats/NBAStats/Models/FavoriteTeam.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using SQLite;
 
 
 namespace NBAStats.Models
 {
     public class FavoriteTeam
     {
-        [MaxLength(50)]
         public string IdFavoriteTeam { get; set; }
-        [MaxLength(40)]
         public string Name { get; set; }
     }
 }
